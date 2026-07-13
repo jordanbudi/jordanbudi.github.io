23848b7bb644413e588dbd8ac7ac242cf8ff2170
Jordan <jordanbudi@gmail.com>
2026-07-12 23:57:37 -0700

    Adding images/thumbnails to everything
    Also added VietVR project

--- js/awards-data.js
+++ js/awards-data.js
@@ -8,27 +8,30 @@ const AWARDS = [
     id: "shine-award",
     title: "Governor's Shine Award",
     year: "2015",
-    tags: ["Teaching"],
+    tags: ["Teaching", "2016"],
     blurb: "Governor of Florida's Shine Award for Distinguished Teaching.",
-    images: ["img/awards/shine-award-1.jpg"]
+    images: ["img/awards/shine-1.jpg"]
   },
   {
     id: "natgeo-arctic",
     title: "National Geographic Grosvenor Teacher Fellow",
-    tags: ["Expedition"],
+    tags: ["Expedition", "2018"],
     blurb: "Selected as a Grosvenor Teacher Fellow and sent on an expedition to the Arctic.",
     images: [
-      "img/awards/natgeo-arctic-1.jpg",
-      "img/awards/natgeo-arctic-2.jpg",
-      "img/awards/natgeo-arctic-3.jpg"
+      "img/awards/natgeo-1.jpg",
+      "img/awards/natgeo-2.jpg",
+      "img/awards/natgeo-3.jpg",
+      "img/awards/natgeo-4.jpg",
+      "img/awards/natgeo-5.jpg",
+      "img/awards/natgeo-6.jpg"
     ]
   },
   {
     id: "ucla-fellowship",
     title: "UCLA Anderson fellowship",
-    tags: ["MBA"],
+    tags: ["MBA","Class of 2022"],
     blurb: "Full-tuition fellowship to attend UCLA Anderson School of Management.",
-    images: ["img/awards/ucla-fellowship-1.jpg"]
+    images: ["img/awards/anderson-1.jpg"]
   },
   {
     id: "poets-quants",
@@ -36,7 +39,7 @@ const AWARDS = [
     year: "2020",
     tags: ["Press"],
     blurb: "Featured in Meet the MBA Class of 2022.",
-    images: ["img/awards/poets-quants-1.jpg"],
+    images: ["img/awards/poets-quants-1.png"],
     linkUrl: "https://poetsandquants.com/2020/12/18/meet-the-mba-class-of-2022-jordan-budisantoso-ucla-anderson/?"
   },
   {
@@ -45,7 +48,16 @@ const AWARDS = [
     year: "2015",
     tags: ["Press"],
     blurb: "Published op-ed: Computer Science Is a Way Out of Poverty.",
-    images: ["img/awards/usnews-oped-1.jpg"],
+    images: ["img/awards/usnews-oped-1.png"],
     linkUrl: "https://www.usnews.com/news/stem-solutions/articles/2015/12/10/op-ed-computer-science-is-a-way-out-of-poverty"
+  },
+  {
+    id: "viceland-vr",
+    title: "Viceland Feature - Beyond the Frame: The New Classroom ",
+    year: "2017",
+    tags: ["Press"],
+    blurb: "Always trying to be at the forefront of the field, I spent 18 months exploring the applications of virtual reality for learning",
+    images: ["img/awards/poets-quants-1.png"],
+    linkUrl: "https://youtu.be/zGGVYT0cMHg?si=PQfTb5EBQU9-wD2N"
   }
 ];
